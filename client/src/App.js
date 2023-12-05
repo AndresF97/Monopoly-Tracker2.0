6f1c83979545bcfa4241a153869bcaedc6833995
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2023-12-05 21:22:58 +0900

    set basic structure for server connection in the App.jsx file

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router , Routes, Route} from "react-router-dom"
+import {ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from "@apollo/client"
+import {setContext} from "@apollo/client/link/context"
 
+const httpLink= createHttpLink({
+  uri:'/graphql'
+})
+const authLink= setContext((_,{headers})=>{
+  const token = localStorage.getItem("id_token")
+  return {
+    headers:{
+      ...headers,
+      auhtorization: token ? `Bearer ${token}` :``
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 function App() {
   return (
+    <ApolloProvider client={client}>
+      {/* <Router> */}
+      <>
+
+    {/* <Routes>  */}
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +44,15 @@ function App() {
         </a>
       </header>
     </div>
+      
+
+      {/* <Route> */}
+        
+      {/* </Route> */}
+    {/* </Routes> */}
+    </>
+    {/* </Router> */}
+    </ApolloProvider>
   );
 }
 
