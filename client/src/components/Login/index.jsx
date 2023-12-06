4ba0cd063b0461d2c9ac89b95f09c5a86124cba0
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2023-12-06 17:13:57 +0900

    added notes to set some functionality in the homepage

---
+++ client/src/components/Login/index.jsx
@@ -0,0 +1,15 @@
+const Login = () =>{
+    return (
+        <>
+            <form>
+                <label>username</label>
+                <input type="text"></input>
+                <label>password</label>
+                <input type="text"></input>
+                <div>
+                    <button>Login</button>
+                </div>
+            </form>
+        </>
+    )
+}
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -3,6 +3,7 @@ const HomePage = ()=>{
     return (
         <>
         <h1>Home Page!</h1>
+        {/* create swicth case to make sure login or sign up are render depending what the user want to do */}
         </>
     )
 }
