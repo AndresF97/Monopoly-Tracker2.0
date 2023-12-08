cbdbf9e41d22e652edc8007aaee92dc0f1495ca7
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2023-12-08 18:47:18 +0900

    if statement set up for homepage will probably add a component to render a log in or sign up

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,14 +1,26 @@
 import Login from "../components/Login"
 import Signup from "../components/SignUp"
+import { useState } from "react"
 
 const HomePage = ()=>{
+    const [createGame, createGameState] = useState(false)
+    const LogNSignSwicthBtn = () =>{}
 
     return (
         <>
         <h1>Home Page!</h1>
+        { createGame ? (
+            <>
+            <Login/>
+            <Signup/>
+            </>
+        ):(
+            <button onClick={()=> createGameState(true)}>Create Game!</button>
+        )
+
+        }
+
         {/* create swicth case to make sure login or sign up are render depending what the user want to do */}
-        <Login/>
-        <Signup/>
         </>
     )
 }
