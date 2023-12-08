d02b63319d2b5965ea9a0f3507168cb491a8d59a
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2023-12-08 20:04:04 +0900

    fixed some bugs with the Auth services and mutation typo

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,12 +1,39 @@
+import {useMutation } from "@apollo/client"
+import { useState } from "react"
+import {LOGIN} from "../../utils/mutations"
+import Auth from '../../utils/auth'
+
 const Login = () =>{
+    const [userForm, userFormState] = useState({email:'',password:''})
+    const [login, {error}] = useMutation(LOGIN)
+    const handleInputChange = (event) =>{
+        const {name, value} = event;
+        console.log(value)
+        userFormState({...userForm, [name]:value});
+    }
+    const handleFormSubmit = async(event)=>{
+        event.preventDefault()
+    }
     return (
         <>
         <h2>Login</h2>
             <form>
                 <label>username</label>
-                <input type="text"></input>
+                <input 
+                type="text"
+                name="email"
+                placeholder="username"
+                onChange={handleInputChange}
+                value={userForm.email}
+                ></input>
                 <label>password</label>
-                <input type="text"></input>
+                <input 
+                type="password"
+                name="password"
+                value={userForm.password}
+                onChange={handleInputChange}
+                placeholder="password"
+                ></input>
                 <div>
                     <button>Login</button>
                 </div>
