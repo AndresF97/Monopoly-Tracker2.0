a267dc28106d8adf93e17f53ea1b7140f01a3b9a
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2023-12-10 18:56:43 +0900

    useMuttaion is set up but not tested yet, will do that next

--- client/src/components/GameForm/index.jsx
+++ client/src/components/GameForm/index.jsx
@@ -1,15 +1,25 @@
 import {useState} from "react"
+import { CREATE_GAME } from "../../utils/mutations"
+import { useMutation } from "@apollo/client"
+
 const GameForm = () =>{
     const [name, nameState] = useState('')
     const handleInputChange = (event) =>{
         const {value} = event.target;
-        console.log(value)
         nameState(value)
+    }
+    const gameFormSubmit = async(event)=>{
+        event.preventDefault();
+        try{
+            console.log(name)
+        }catch(err){
+            console.error(err)
+        }
     }
     return (
         <>
         <h3>Game Form</h3>
-        <form>
+        <form onSubmit={gameFormSubmit}>
             <label>Name of Game</label>
             <input 
             placeholder="Name of game here"
