3fa37fd67b247ed78c5f4d7a7de453e046707db5
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2023-12-11 19:18:45 +0900

    swicth page if statement works

---
+++ client/src/components/PlayerForm/index.jsx
@@ -0,0 +1,7 @@
+const PlayerForm =  ()=>{
+    return (
+        <h1>Player Form</h1>
+    )
+}
+
+export default PlayerForm;
--- client/src/pages/GameHistory.jsx
+++ client/src/pages/GameHistory.jsx
@@ -1,39 +1,49 @@
 import { ME } from "../utils/queries"
 import { useState } from "react"
 import { useQuery } from "@apollo/client"
+import PlayerForm from "../components/PlayerForm"
 const GameHistory = () => {
 
     const { loading, data } = useQuery(ME, {
         fetchPolicy: "no-cache"
     })
     const gameList = data?.me?.gameMaster || []
-    const [swicthPage, swicthPageState]  = useState(false)
+    const [swicthPage, swicthPageState]  = useState(true)
     const [currentGameId, currentGameIdState] = useState('')
-    const swicthToPlayerForm = ()=>{
-        console.log('hello')
+    const swicthToPlayerForm = (event)=>{
+        swicthPageState(false)
+        currentGameIdState(event.target.getAttribute("data-id"))
     }
     return (
         <>
-            <h1>Game History</h1>
-            {loading ? (
-                <h1>loading</h1>
-            ) : (
-                <section>
-                    <ul>
-                    {gameList?.map((game) => {
-                        // add link to create a new page for adding/updating player
-                        return (
-                            <li key={game._id} data-id={game._id} onClick={swicthToPlayerForm}>
-                                <h2 >{game.name}</h2>
-                            </li>
-                        )
-
-                    })}
-                    </ul>
-                </section>
-            )}
-            {/* create swicth case to check for if user wants to change part of the page to a diffrent component.
+         <h1>Game History</h1>
+        {swicthPage?(
+            <>
+                {loading ? (
+                    <h1>loading</h1>
+                ) : (
+                    <section>
+                        <ul>
+                        {gameList?.map((game) => {
+                            // add link to create a new page for adding/updating player
+                            return (
+                                <li key={game._id} onClick={(event)=>{swicthToPlayerForm(event)}}>
+                                    <h2 data-id={game._id}>{game.name}</h2>
+                                </li>
+                            )
+    
+                        })}
+                        </ul>
+                    </section>
+                )}
+                </>
+        ):(
+            <section>
+             {/* create swicth case to check for if user wants to change part of the page to a diffrent component.
              past props of the game id and the state for the page to that commponent */}
+            <PlayerForm/>
+            </section>
+        )}
 
         </>
     )
