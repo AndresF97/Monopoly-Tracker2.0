39b9c90a2e806f961c9e5d3cbf07b2e630a7d72e
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2023-12-26 21:55:09 +0900

    added new functionality to update a state

--- client/src/pages/GameHistory.jsx
+++ client/src/pages/GameHistory.jsx
@@ -9,16 +9,20 @@ const GameHistory = () => {
     })
     const gameList = data?.me?.gameMaster || []
     const [currentGameId, currentGameIdState] = useState('')
-    const [currentGameName, setCurrentGame] = useState('')
+    const [currentGameName, setCurrentGameName] = useState('')
     let [createPlayeForm,setCreatePlayerForm] = useState([]) 
 
     const swicthToPlayerForm = (event)=>{
-        console.log(createPlayeForm[0])
-        if(createPlayeForm.length > 5){
-            alert("Thats to many player forms")
-            return
-        }createPlayeForm.push(<PlayerForm key={currentGameId} currentGameId={currentGameId}/>)
-        setCreatePlayerForm(createPlayeForm)
+        // console.log(createPlayeForm[0])
+        // if(createPlayeForm.length > 5){
+        //     alert("Thats to many player forms")
+        //     return
+        // }createPlayeForm.push(<PlayerForm key={currentGameId} currentGameId={currentGameId}/>)
+        // setCreatePlayerForm(createPlayeForm)
+    }
+    const stateCurrentGameInfo = (event)=>{
+        let gameName = event.target.textContent
+        let gameId = event.target.getAttribute('data-id')
     }
     return (
         <>
@@ -34,7 +38,7 @@ const GameHistory = () => {
                                  {gameList?.map((game) => {
                                      // add link to create a new page for adding/updating player
                                      return (
-                                         <li key={game._id}>
+                                         <li key={game._id} onClick={(event)=>{stateCurrentGameInfo(event)}}>
                                              <h2 data-id={game._id}>{game.name}</h2>
                                          </li>
                                      )
