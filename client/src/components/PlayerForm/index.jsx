33d2102e8fb1460ae4d36d290d4904c45a890c49
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2023-12-27 19:24:16 +0900

    player form functionality works so afr for each player different name are logged

--- client/src/components/PlayerForm/index.jsx
+++ client/src/components/PlayerForm/index.jsx
@@ -1,21 +1,44 @@
+import { useState } from "react";
+
 const PlayerForm =  ({currentGameId})=>{
+    const [playerForm, setPlayerForm] = useState({name:'',token:"",money:'',position:'',gameId:currentGameId})
+    const playerSubmitForm = (event)=>{
+        event.preventDefault()
+        console.log(playerForm.name)
+    }
+    const handleInputChange= (event)=>{
+        const {name, value } = event.target
+        setPlayerForm({...playerForm, [name]:value})
+    }
     console.log(currentGameId)
     return (
         <section>
             <h3>Player Form</h3 >
-            <form>
+            <form onSubmit={(event)=>{playerSubmitForm(event)}}>
                 <label>Name of Player:</label>
                 <br></br>
-                <input type="text"></input>
+                <input 
+                type="text"
+                name="name"
+                value={playerForm.name}
+                onChange={handleInputChange}
+                ></input>
                 <br></br>
                 <label>Token of Player:</label>
-                <input type="text"></input>
+                <input 
+                type="text"
+                ></input>
                 <br></br>
                 <label>Money:</label>
-                <input type="text"></input>
+                <input 
+                type="text"
+                ></input>
                 <br></br>
                 <label>Position:</label>
-                <input type="text"></input>
+                <input 
+                type="text"
+                ></input>
+                <button type="submit" >Submit</button>
             </form>
         </section>
         
