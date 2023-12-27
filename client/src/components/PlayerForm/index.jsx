3a4dcf543ad2b4704fd1de0d77862bb87545bb80
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2023-12-27 18:56:49 +0900

    player form is been render

--- client/src/components/PlayerForm/index.jsx
+++ client/src/components/PlayerForm/index.jsx
@@ -1,8 +1,24 @@
 const PlayerForm =  ({currentGameId})=>{
     console.log(currentGameId)
     return (
-        <h3>Player Form</h3 >
-
+        <section>
+            <h3>Player Form</h3 >
+            <form>
+                <label>Name of Player:</label>
+                <br></br>
+                <input type="text"></input>
+                <br></br>
+                <label>Token of Player:</label>
+                <input type="text"></input>
+                <br></br>
+                <label>Money:</label>
+                <input type="text"></input>
+                <br></br>
+                <label>Position:</label>
+                <input type="text"></input>
+            </form>
+        </section>
+        
     )
 }
 
