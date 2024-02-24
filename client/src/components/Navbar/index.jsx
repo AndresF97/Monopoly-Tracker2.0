169a28aec098d0e2f85326a8b2b86482411f3ce6
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2024-02-24 21:58:38 +0900

    nav center logo added

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 import Auth from "../../utils/auth"
+import NavLogo from "../../assets/images/MTNavLogo.png"
 const Navbar = () => {
     return (
         <header>
@@ -9,7 +10,7 @@ const Navbar = () => {
                     <Link class="inline-block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white" to="/">Home</Link>
                 </li>
                 <li className="mr-5">
-                    <Link class="inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4" to="/" >Monopoly Tracker</Link>
+                    <Link class="inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4" to="/" ><img className="h-20 w-30"src={NavLogo}/></Link>
                 </li>
                 <li>
                     
