5fa14326435e5ee5cdc3cf0967fea930fcdc8892
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2024-03-03 18:53:13 +0900

    will comeback to the navbar issue later will continue to work on the add Game for next

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -4,6 +4,7 @@ import NavLogo from "../../assets/images/MTNavLogo.png"
 const Navbar = () => {
     return (
         <header>
+            {/* TODO */}
             {/* Might need to comeback here and do this */}
             <nav>
             <ul class="flex justify-between  bg-neutral-200 text-center text-white dark:bg-neutral-600">
@@ -13,13 +14,13 @@ const Navbar = () => {
                 <li>
                     <Link to="/" ><img className="h-20 w-30"src={NavLogo}/></Link>
                 </li>
-                <li className="inline-block mt-3 text-white">
+                <li className="inline-block mt-3 text-white ml-[10%]">
                 {Auth.loggedIn() ? (
-                        <>
+                        <span>
                             <Link className="py-2 px-4 text-gray-400"Link to="/History">History</Link>
                             {/* Log out functionality */}
                             <Link onClick={Auth.logOut} className="py-2 px-4 text-gray-400">Log out</Link>
-                        </>
+                        </span>
                     ) : (
                         <>
                         </>
