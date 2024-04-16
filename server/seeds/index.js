a1826977475b5e7a12df6ff9a261c1f650e7a9a9
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2024-04-16 18:04:25 +0900

    updated package

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -27,47 +27,47 @@ db.once("open",async()=>{
     // This works but might need to work in future developemnt that way there are no duplicates in the array
     // NOTE: COULD CREATE A VAR TO MANIPULATE THE DATA TO EREASE AS WE DELETE
     
-    for(var i =0; i < playerData.length; i++){
-       newPlayers.push( await Player.create({
-            name:playerData[i].name,
-            token:playerData[i].token,
-            playerPropreties:propertySeeder[Math.floor(Math.random()*propertySeeder.length)]
-        })
-       )
-    }
+    // for(var i =0; i < playerData.length; i++){
+    //    newPlayers.push( await Player.create({
+    //         name:playerData[i].name,
+    //         token:playerData[i].token,
+    //         playerPropreties:propertySeeder[Math.floor(Math.random()*propertySeeder.length)]
+    //     })
+    //    )
+    // }
         
     console.log("------------- ///////////// ---------------\n")
 
     console.log('////    ----- GAMER SEEDS AND PLAYER SEEDS CONNECT TABLE -----   ////\n')
-    for(var i = 0; i < gameData.length;i++){
-        // maybe we create an array of random play
-        const randomPlayerId = newPlayers.map((player,i)=>{
-            if(i < 3){
-                return newPlayers[Math.floor(Math.random()*newPlayers.length)]._id
-            }
-        }).filter(notUndefined => notUndefined!==undefined)
-        // console.log(randomPlayerId)
-        newGames.push(await Game.create({
-            name:gameData[i].name,
-            savedPlayers:randomPlayerId
-        })
-        )
+    // for(var i = 0; i < gameData.length;i++){
+    //     // maybe we create an array of random play
+    //     const randomPlayerId = newPlayers.map((player,i)=>{
+    //         if(i < 3){
+    //             return newPlayers[Math.floor(Math.random()*newPlayers.length)]._id
+    //         }
+    //     }).filter(notUndefined => notUndefined!==undefined)
+    //     // console.log(randomPlayerId)
+    //     newGames.push(await Game.create({
+    //         name:gameData[i].name,
+    //         savedPlayers:randomPlayerId
+    //     })
+    //     )
 
-    }
-    console.log('////    ----- USER SEEDS TABLE -----   ////\n')
-    for(var i = 0; i < userData.length;i++){
-        const randomGamesId = newGames.map((player,i)=>{
-            if(i < 1){
-                return newGames[Math.floor(Math.random()*newGames.length)]._id
-            }
-        }).filter(notUndefined => notUndefined!==undefined)
-        await User.create({
-            username:userData[i].username,
-            email:userData[i].email,
-            password:userData[i].password,
-            gameMaster:randomGamesId
-        })
-    }
+    // }
+    // console.log('////    ----- USER SEEDS TABLE -----   ////\n')
+    // for(var i = 0; i < userData.length;i++){
+    //     const randomGamesId = newGames.map((player,i)=>{
+    //         if(i < 1){
+    //             return newGames[Math.floor(Math.random()*newGames.length)]._id
+    //         }
+    //     }).filter(notUndefined => notUndefined!==undefined)
+    //     await User.create({
+    //         username:userData[i].username,
+    //         email:userData[i].email,
+    //         password:userData[i].password,
+    //         gameMaster:randomGamesId
+    //     })
+    // }
     console.log("------------- ///////////// ---------------\n")
     console.log("////    ----- ????PLAYER PROPERTY SEED SEEDS TABLE?? NEED TO MAKE SURE EVERY THING ELSE WORKS -----   ////\n")
     }catch(err){
